9215f835f87ceb3a1e2b895d6fb22af435ac7f73
Jairfsj <jairfsj@gmail.com>
2022-01-25 22:28:22 -0300

    modified : scripts

---
+++ python_scripts/ex.2.py
@@ -0,0 +1,6 @@
+# Crie um programa que, voçê digite um numero e outro numero  e saia uma a soma entre eles .
+
+n1 = int(input('Digite um numero'))
+n2 = int(input('Digite outro numrero'))
+s = n1 + n2
+print('A soma de entre {} e {}, é igual à {}'.format(n1, n2, s))
