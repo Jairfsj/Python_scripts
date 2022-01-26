f3b3de73d031e3fd025ea2e3b2d0c47eada4d263
Jairfsj <jairfsj@gmail.com>
2022-01-25 22:36:45 -0300

    new : scripts

---
+++ python_scripts/ex.4.py
@@ -0,0 +1,9 @@
+# Faça um programa que leia um numero inteiro e mostre na tela o seu sucessor e seu antecessor .
+
+# n = int(input('Digite un número: '()
+# print(' Analisando o valor{}, seu antecessor é {} e o sucessor é {}'.format(n, (n-1), (n+1)))
+
+n = int(input('Digite um número: '))
+a = n - 1
+s = n + 1
+print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n, a, s))
