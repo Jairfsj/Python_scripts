6e9069b08ba16c9d10a43ec34dd14bdfa8774fe4
Jairfsj <jairfsj@gmail.com>
2022-01-25 22:43:00 -0300

    new : scripts

---
+++ python_scripts/ex.6.py
@@ -0,0 +1,6 @@
+# Desenvolva um programa que leia as notas de um aluno, calcula e mostra a sua média.
+
+n1 = float(input('Primeira nota do aluno : '))
+n2 = float(input('Segunda nota do aluno : '))
+média = n1 + n2 / 2
+print('A média entre {} e {} é igual a {}.'.format(n1, n2, média))
